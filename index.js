826d9d1eb4ab042948cfc57e734e28680cbe7890
Daniel Vizueta <vizuetadaniel85@gmail.com>
2022-05-23 16:50:08 -0500

    changesmade2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const arrayOfTodos = [
     {
     "userId": 14,
     "id": 1,
-    "title": "delectus aut autem",
+    "title": "delectus aut autem1",
     "completed": false
 },
 {
@@ -17,10 +17,18 @@ function populateTodos(){
     // document.getElementById("todo-list");
 let olElement = document.getElementById("todo-list");
 // document.createElement("li")
-let liElement = document.createElement("li");
-let textNode = document.createTextNode(arrayOfTodos[0].title);
-liElement.appendChild(textNode);
-olElement.appendChild(liElement);
-}
+// let liElement = document.createElement("li");
+// let textNode = document.createTextNode(arrayOfTodos[0].title);
+// liElement.appendChild(textNode);
+// olElement.appendChild(liElement);
+
+for (let i = 0; i < arrayOfTodos.length; i++){
+    // let olElement = document.getElementById("todo-list");
+    document.createElement("li")
+    let liElement = document.createElement("li");
+    let textNode = document.createTextNode(arrayOfTodos[i].title);
+    liElement.appendChild(textNode);
+    olElement.appendChild(liElement);  
+}}
 console.log(arrayOfTodos[0].userId) // => 14
 console.log(arrayOfTodos[1].userId) // => 20
